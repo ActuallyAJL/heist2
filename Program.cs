07c7c5c68c040b71842c2e89c12017fb9832fc6f
Houston-Smith <houstonbsmith@gmail.com>
2022-06-08 15:15:53 -0500

    Points 3 and 4 done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace heist2
 {
@@ -6,7 +7,46 @@ namespace heist2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            List<IRobber> rolodex = new List<IRobber>()
+            {
+                new Muscle()
+                {
+                    Name = "Alex",
+                    SkillLevel = 100,
+                    PercentageCut = 30
+                },
+                 new LockSpecialist()
+                {
+                    Name = "Houston",
+                    SkillLevel = 100,
+                    PercentageCut = 30
+                },
+                 new Hacker()
+                {
+                    Name = "Nathan",
+                    SkillLevel = 100,
+                    PercentageCut = 30
+                },
+                 new Muscle()
+                {
+                    Name = "Olivia",
+                    SkillLevel = 100,
+                    PercentageCut = 30
+                },
+                 new Hacker()
+                {
+                    Name = "Jordan",
+                    SkillLevel = 100,
+                    PercentageCut = 30
+                },
+                 new LockSpecialist()
+                {
+                    Name = "Steve",
+                    SkillLevel = 100,
+                    PercentageCut = 30
+                }
+            };
         }
     }
 }
