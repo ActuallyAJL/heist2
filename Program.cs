c8e98a4ee9e961edbc050388a06bca2a6047a256
Olivia Lind <eo.lind@gmail.com>
2022-06-09 10:29:28 -0500

    fixed typos

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace heist2
     {
         static void Main(string[] args)
         {
-        
+
             List<IRobber> rolodex = new List<IRobber>()
             {
                 new Muscle()
@@ -50,52 +50,54 @@ namespace heist2
 
             Console.WriteLine($"Current number of operatives in the Rolodex: {rolodex.Count}");
 
-            AddNewMember()
-            
+            AddNewMember();
+
+
             void AddNewMember()
             {
                 Console.WriteLine("Please enter the name of a new crew member");
                 string newMemName = Console.ReadLine();
-                
-                if(newMemName != "")
+
+                if (newMemName != "")
                 {
-                Console.WriteLine(@"Please enter the member's specialty
+                    Console.WriteLine(@"Please enter the member's specialty
                 Enter 1 for Hacker
                 Enter 2 for Muscle
-                Enter 3 for LockSpecialist")
+                Enter 3 for LockSpecialist");
                 int newMemSpecialty = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter the new member's skill level (1-100)");
-                int newMemSkillLevel = int.Parse(Console.Readline());
-                Console.WriteLine("Enter the percentage cut for the new member");
-                int newMemPercentageCut = int.Parse(Console.Readline());
+                    Console.WriteLine("Enter the new member's skill level (1-100)");
+                    int newMemSkillLevel = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the percentage cut for the new member");
+                    int newMemPercentageCut = int.Parse(Console.ReadLine());
 
                     switch (newMemSpecialty)
                     {
                         case 1:
-                        {
-                            rolodex.Add(new Hacker {Name = newMemName, PercentageCut = newMemPercentageCut, SkillLevel = newMemSkillLevel});
-                            break;
-                        }
+                            {
+                                rolodex.Add(new Hacker { Name = newMemName, PercentageCut = newMemPercentageCut, SkillLevel = newMemSkillLevel });
+                                break;
+                            }
                         case 2:
-                        {
-                            rolodex.Add(new Muscle {Name = newMemName, PercentageCut = newMemPercentageCut, SkillLevel = newMemSkillLevel});
-                            break;
-                        }
+                            {
+                                rolodex.Add(new Muscle { Name = newMemName, PercentageCut = newMemPercentageCut, SkillLevel = newMemSkillLevel });
+                                break;
+                            }
                         case 3:
-                        {
-                            rolodex.Add(new LockSpecialist {Name = newMemName, PercentageCut = newMemPercentageCut, SkillLevel = newMemSkillLevel});
-                            break;
-                        }
+                            {
+                                rolodex.Add(new LockSpecialist { Name = newMemName, PercentageCut = newMemPercentageCut, SkillLevel = newMemSkillLevel });
+                                break;
+                            }
                     }
 
                     Console.WriteLine("Do you want to add another member? (Y/N)");
-                    string userResponse - Console.ReadLine();
-                    if(userResponse.ToLower() == "y")
+                    string userResponse = Console.ReadLine();
+                    if (userResponse.ToLower() == "y")
                     {
                         AddNewMember();
                     }
                 }
-        
+
             }
-    }   }
+        }
+    }
 }
